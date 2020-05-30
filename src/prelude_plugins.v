efcf7931af5a55caa409e43d31f8858639a3184c
Pierre Roux <pierre@roux01.fr>
2020-05-30 18:58:40 +0200

    Adapt to coq/coq#12218

--- src/prelude_plugins.v
+++ src/prelude_plugins.v
@@ -3,4 +3,4 @@ Require Import prelude.
 (** Plugins loaded initially *)
 Declare ML Module "ssrmatching_plugin".
 Declare ML Module "ssreflect_plugin".
-Declare ML Module "numeral_notation_plugin".
+Declare ML Module "number_string_notation_plugin".
